40eba452969af74be895fbdef6f0d4777ee2a50b
Alzemand <alzemand@outlook.com>
2019-04-19 00:01:29 -0300

    Jokenpo

---
+++ Jokenpo/app/src/main/java/com/example/jokenpo/MainActivity.java
@@ -0,0 +1,75 @@
+package com.example.jokenpo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void selecionarPedra(View view){
+        this.opcaoSelecionada("pedra");
+    }
+
+    public void selecionarPapel(View view){
+        this.opcaoSelecionada("papel");
+    }
+
+    public void selecionarTesoura(View view){
+        this.opcaoSelecionada("tesoura");
+    }
+
+    public void opcaoSelecionada(String escolhaUsuario){
+
+        ImageView imageResultado = findViewById(R.id.imageResult);
+        TextView textoResultado = findViewById(R.id.textResult);
+
+        int numero = new Random().nextInt(3);
+        String[] opcoes = {"pedra", "papel", "tesoura"};
+        String escolhaApp = opcoes[numero];
+
+        switch (escolhaApp){
+            case "pedra":
+                imageResultado.setImageResource(R.drawable.pedra);
+                break;
+            case "papel":
+                imageResultado.setImageResource(R.drawable.papel);
+                break;
+            case "tesoura":
+                imageResultado.setImageResource(R.drawable.tesoura);
+                break;
+        }
+        //Condições que fazem o app ganhador
+        if (
+                (escolhaApp == "pedra" && escolhaUsuario == "tesoura") ||
+                (escolhaApp == "papel" && escolhaUsuario == "pedra") ||
+                (escolhaApp == "tesoura" && escolhaUsuario == "papel")
+        ){//App ganhador
+        textoResultado.setText("Você perdeu pro seu celular");
+        }else if(
+                (escolhaUsuario == "pedra" && escolhaApp == "tesoura") ||
+                (escolhaUsuario == "papel" && escolhaApp == "pedra") ||
+                (escolhaUsuario == "tesoura" && escolhaApp == "papel")
+
+        ){//Usuário ganhador
+        textoResultado.setText("Você ganhou do seu celular");
+        }else{
+        textoResultado.setText("Deu empate!");
+
+        }
+
+
+
+    }
+
+
+}
